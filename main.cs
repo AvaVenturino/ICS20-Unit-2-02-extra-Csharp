9de116dc10b86c1ef2de5592de71b4faf65f7633
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-12 00:44:01 +0000

    Changed wording

--- main.cs
+++ main.cs
@@ -9,7 +9,7 @@ class Program
 {
     public static void Main (string[] args) 
     {
-      Console.WriteLine ("The dimentions are 5 cm and 3 cm");
+      Console.WriteLine ("The dimensions are 5 cm and 3 cm");
       Console.WriteLine("The area is = " + (3 * 5) + "cm²");
       Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm"); 
 
