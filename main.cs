1421aea07217df775ac2c5183778cad806ef29a4
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-12 01:22:48 +0000

    Fixed spacing

--- main.cs
+++ main.cs
@@ -5,13 +5,14 @@
 
 using System;
 
-public class Program 
+public class Program
 {
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
         Console.WriteLine("The dimensions are 5 cm and 3 cm");
         Console.WriteLine("The area is = " + (3 * 5) + "cm²");
-        Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm"); 
+        Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm");
+        
         Console.WriteLine("\nDone.");
     }
 }
