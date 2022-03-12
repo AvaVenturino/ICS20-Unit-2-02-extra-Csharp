e2a877561c2773585e443cd3b18c79189a17f865
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-12 01:36:23 +0000

    added main cs

---
+++ main.cs
@@ -0,0 +1,17 @@
+// Created by: Ava Venturino
+// Created on: Mar 2022
+// 
+// This program does basic math
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("The dimensions are 5 cm and 3 cm");
+        Console.WriteLine("The area is = " + (3 * 5) + "cm²");
+        Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm");
+        Console.WriteLine("\nDone.");
+    }
+}
