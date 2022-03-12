e7538f6483d213ea1a132b8e9fce010e60ab28ae
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-12 00:56:54 +0000

    Changed spacing

--- main.cs
+++ main.cs
@@ -7,12 +7,13 @@ using System;
 
 class Program 
 {
-      public static void Main (string[] args) 
-      {
-       Console.WriteLine ("The dimensions are 5 cm and 3 cm");
-       Console.WriteLine("The area is = " + (3 * 5) + "cm²");
-       Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm"); 
+  
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine ("The dimensions are 5 cm and 3 cm");
+        Console.WriteLine("The area is = " + (3 * 5) + "cm²");
+        Console.WriteLine("The perimeter is = " + (5 + 5 + 3 + 3) + "cm"); 
 
-       Console.WriteLine("\nDone.");
+        Console.WriteLine("\nDone.");
     }
 }
